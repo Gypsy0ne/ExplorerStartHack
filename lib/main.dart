5d70cb19609ad06784205905dfd955e4f7aa99d6
MarekSledziewski <m.sledziewski0@gmail.com>
2021-03-20 04:30:09 +0100

    kom

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home:Scaffold
-      (body: HomeScreen()
+      (body: HomeScreenProvider()
       )
     );
   }
