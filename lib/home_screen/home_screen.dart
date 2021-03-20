20590885138c54b5cd2754c43bc798b93f9a403d
MarekSledziewski <m.sledziewski0@gmail.com>
2021-03-20 01:09:33 +0100

    com

--- lib/home_screen/home_screen.dart
+++ lib/home_screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -10,10 +11,41 @@ class HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("SBB explorer"),
-      ),
-      body: Container(child: Text("Home screen lies here")),
-    );
+        appBar: AppBar(
+          title: Text("SBB explorer"),
+        ),
+        body: Column(children:
+            [buildSearchField(),
+
+        ListView(
+
+
+        )]));
   }
 }
+
+
+Widget buildSearchFieldWithFadeInAnimation() =>
+    TweenAnimationBuilder(
+      child: buildSearchField(),
+      tween: Tween<double>(begin: 0, end: 1),
+      duration: Duration(seconds: 1),
+      builder: (BuildContext context, double _value, Widget child) {
+        return Opacity(
+          opacity: _value,
+          child: Padding(
+              padding: EdgeInsets.only(top: _value * 20), child: child),
+        );
+      },
+    );
+
+Widget buildSearchField() =>
+  TextField(
+
+      maxLength: 20,
+      maxLengthEnforcement: MaxLengthEnforcement.enforced,
+      decoration: InputDecoration(
+          border: InputBorder.none,
+          prefixIcon: Icon(Icons.search),
+          hintText: 'Search'));
+
